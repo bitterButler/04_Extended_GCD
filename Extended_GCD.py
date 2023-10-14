d9fdc99e86db9f1690e3920f4046ee580067c2a2
bitterButler <b.hudson.mark@gmail.com>
2023-10-14 11:13:32 +0200

    simpleGCD, and Extended version is working, BUT with only a few inputs like: (240, 46) (333, 56) (112, 33) (208, 128). IndexError and looping thru in second part maybe the problem

--- Extended_GCD.py
+++ Extended_GCD.py
@@ -1,54 +1,4 @@
-# def extended_GCD(a, b):  # a, b is the number which user will input
-#
-#    UserInput_A = a
-#    UserInput_B = b
-#
-#    Non_modified_A = a  # for saving the user inputs, for print(f"").
-#    Non_modified_B = b
-#    Q = Non_modified_A // Non_modified_B
-#    x = 1  # not used yet
-#    y = 0  # not used yet
-#    Index = 0
-#
-#    listforXY = list()
-#    while b > 0:
-#        if Index == 0:
-#            print(f"{Index}: {a} ")
-#            listforXY.append(a)
-#            Index += 1
-#        if Index == 1:
-#            print(f"{Index}: {b} ")
-#            listforXY.append(b)
-#        R = a % b
-#        if b > R:
-#            Index += 1
-#            if R == 0:  # needed this if-statement, or else there will be "ZeroDivisionError: integer division or modulo by zero"
-#                print(
-#                    f"{Index} | {Non_modified_A} / {Non_modified_B} = {Q} | Rest = {R} | ")
-#
-#                print(
-#                    f"{Index+1} | Because Rest = 0 -> No Zero Division (example: x / 0) | We found the GCD({UserInput_A}, {UserInput_B}) = {b}")
-#                break
-#            a = b
-#            b = R
-#        print(f"{Index} | {Non_modified_A} / {Non_modified_B} = {Q} | Rest = {R} | ")
-#        print(listforXY)
-#        Non_modified_A = Non_modified_B
-#        Non_modified_B = R
-#        Q = Non_modified_A // Non_modified_B
-#
-#    print("")
-#    while Index >= 0:
-#        x_new = y
-#        y_new = x - (Q * y)
-#        print(f"{Index} | x = {x_new} | y = {y_new} ")
-#        Index -= 1
-#
-#
-# extended_GCD(a=int(input("a= ")), b=int(input("b= ")))
 
-
-#####################################################
 def extended_GCD(a, b):
     UserInput_A = a
     UserInput_B = b
@@ -57,14 +7,9 @@ def extended_GCD(a, b):
     Non_modified_B = b
     Q = Non_modified_A // Non_modified_B
     Index = 0
-
+######################## START OF simpleGCD ########################
     Q_list = []
     while b > 0:
-        '''if Index == 0:
-            print(f"{Index}: {a} ")
-            Index += 1
-        if Index == 1:
-            print(f"{Index}: {b} ")'''
         R = a % b
         if b > R:
             Index += 1
@@ -84,34 +29,38 @@ def extended_GCD(a, b):
         Non_modified_A = Non_modified_B
         Non_modified_B = R
         Q = Non_modified_A // Non_modified_B
+######################## END OF simpleGCD ########################
 
-    # print(Rest_list)  # just for testing purpose.
+    # just for testing purpose.
     print("")
     print("Q-list reversed:", Q_list[::-1])
+    Q_listREVERSE = Q_list[::-1]
     print("")
+    ######################## START OF EXTENDED PART (x, y) ########################
     x = 1
     y = 0
     Index = 0
-    while Index <= 6:  # index = 6
+    i = 0
+    while Index <= 5:
         if Index == 0:
-            print(f"{Index} | x = {x} | y = {y}")  # x = 1, y = 0
-            Index += 1  # index 5 lesz
+            # x = 1, y = 0
+            print(
+                f"{Index} | x = {x} | y = {y} |")
+            Index += 1  # index 1
+            x_new = y  # xnew = 0
+            y_new = x - (Q_listREVERSE[0] * x_new)
         x_new = y  # xnew = 0
-        y_new = x - (Q_list[(Index-2)] * x_new)
-        print(f"{Index} | x = {x_new} | y = {y_new}")
-        Index += 1
+        y_new = x - (Q_listREVERSE[(Index-1)] * x_new)
         x, y = x_new, y_new
-
-    '''while Index >= 1:   
-        if (Index+1) == 6:
-            print(f"{Index} | x = {x} | y = {y}")
-            Index -= 1
-            x_new = y
-            y_new = x - (Q * y)
-        x_new = y
-        y_new = x - (Q * y)
-        print(f"{Index} | x = {x_new} | y = {y_new}")
-        Index -= 1'''
+        print(
+            f"{Index} | x = {x_new} | y = {y_new} | Q_listReverse[{Index-1}]: {Q_listREVERSE[Index-1]}")
+        Index += 1
+    print(
+        f"\na*x + b*y = GCD(a,b)\na= {UserInput_A}, b= {UserInput_B}, GCD(a,b)={b}\n--> {UserInput_A}*({x_new}) + {UserInput_B}*({y_new}) = {b}\n--> {UserInput_A*x_new} + {UserInput_B*y_new} = {(UserInput_A*x_new) + (UserInput_B*y_new)}")
 
 
 extended_GCD(a=int(input("a= ")), b=int(input("b= ")))
+
+'''after testing with different inputs, it does need some fixes.
+   - one of the issues is the looping, or indexing IN THE SECOND, EXTENDED PART. -> right now it works for numbers like (240, 46) (333, 56) (112, 33) (208, 128)
+   - second part needs fix, for looping thru everything. it throws IndexErrors. So because it didnt went thru all of it, the last print() statement, where it does multiplication, its wrong'''
